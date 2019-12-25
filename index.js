86e433896ce90a4b05eabb4b6495bea6ab1625f8
RohanAdhia <rohanadhia@gmail.com>
2019-12-25 23:57:54 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,18 +1,25 @@
 function randomNumberGen() {
   var a = Math.random() * 6;
-  return "images/dice"+(Math.floor(a) + 1)+".png";
+  return "images/dice" + (Math.floor(a) + 1) + ".png";
 }
 
-document.getElementsByTagName("img")[0].setAttribute("src",randomNumberGen());
-document.getElementsByTagName("img")[1].setAttribute("src",randomNumberGen());
 
-var player1 = document.getElementsByTagName("img")[0].getAttribute("src").slice(11,12);
-var player2 = document.getElementsByTagName("img")[1].getAttribute("src").slice(11,12);
+function diceProcess() {
 
-if (player1 > player2) {
-  document.querySelector("h1").innerText = "🚩 Player 1 Wins!";
-} else if (player1 === player2){
-  document.querySelector("h1").innerText = "Draw!";
-} else {
-  document.querySelector("h1").innerText = "Player 2 Wins! 🚩";
+  document.getElementsByTagName("img")[0].setAttribute("src", randomNumberGen());
+  document.getElementsByTagName("img")[1].setAttribute("src", randomNumberGen());
+
+  var player1 = document.getElementsByTagName("img")[0].getAttribute("src").slice(11, 12);
+  var player2 = document.getElementsByTagName("img")[1].getAttribute("src").slice(11, 12);
+
+  if (player1 > player2) {
+    var b = document.querySelector("h1").innerText = "🚩 Player 1 Wins!";
+    return b;
+  } else if (player1 === player2) {
+    var c = document.querySelector("h1").innerText = "Draw!";
+    return c;
+  } else {
+    var d = document.querySelector("h1").innerText = "Player 2 Wins! 🚩";
+    return d;
+  }
 }
